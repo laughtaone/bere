42da18382740e15c8c603f4442fdf0ae9e960e57
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2024-06-09 22:28:41 +0900

    regularly commit (settings)

--- lib/settings_display.dart
+++ lib/settings_display.dart
@@ -38,7 +38,26 @@ class SettingsPage extends StatelessWidget {
           platform: DevicePlatform.iOS,
           sections: [
             SettingsSection(
-              title: const Text('Common'),
+              title: Text(
+                'このアプリについて',
+                style: SettingTitleTextStyle.myTextStyle,
+              ),
+              tiles: <SettingsTile>[
+                SettingsTile(
+                  leading: const Icon(Icons.info),
+                  title: const Text('アプリのバージョン'),
+                  value: Text(
+                    'aa',
+                    style: SettingValueTextStyle.myTextStyle,
+                  )
+                )
+              ],
+            ),
+            SettingsSection(
+              title: Text(
+                'Common',
+                style: SettingTitleTextStyle.myTextStyle,
+              ),
               tiles: <SettingsTile>[
                 SettingsTile.navigation(
                   leading: const Icon(Icons.language),
@@ -58,9 +77,48 @@ class SettingsPage extends StatelessWidget {
                 ),
               ],
             ),
+            SettingsSection(
+              title: Text(
+                'このアプリについて',
+                style: SettingTitleTextStyle.myTextStyle,
+              ),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: const Icon(Icons.person),
+                  title: const Text('開発者のX'),
+                  value: const Text('@suupusoup'),
+                  onPressed: (context) {
+                    // 画面遷移処理
+                  },
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {
+                    // トグル切り替え処理
+                  },
+                  initialValue: true,
+                  leading: const Icon(Icons.format_paint),
+                  title: const Text('Enable custom theme'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+
+class SettingTitleTextStyle {
+  static final TextStyle myTextStyle = TextStyle(
+    fontSize: 17,
+    color: Colors.white,
+  );
+}
+
+class SettingValueTextStyle {
+  static final TextStyle myTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 17,
+  );
 }
