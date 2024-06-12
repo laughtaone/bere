bb856b06c8df2807c63dd4d15bca777a90df90f2
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2024-06-12 20:33:32 +0900

    質問ウィジェット改良

--- lib/settings_qanda_display.dart
+++ lib/settings_qanda_display.dart
@@ -53,7 +53,7 @@ class QandAPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              QAWidget(
+              qaWidget(
                 qText: '質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文質問文',
                 aText: '回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文回答文',
               ),
@@ -64,49 +64,33 @@ class QandAPage extends StatelessWidget {
     );
   }
 
-  SettingsTile QMethod(String qText) {
-    return SettingsTile(
-      leading: Icon(Icons.help),
-      title: Text(qText),
-      onPressed: null,
-    );
-  }
-
-  SettingsTile AMethod(String aText) {
-    return SettingsTile(
-      leading: Icon(Icons.hdr_auto),
-      title: Text(aText),
-      onPressed: null,
-    );
-  }
-
 }
 
 
-class SettingTitleTextStyle {
-  static final TextStyle myTextStyle = TextStyle(
-    fontSize: 17,
-    color: Colors.white,
-    fontWeight: FontWeight.bold,
-  );
-}
+// class SettingTitleTextStyle {
+//   static final TextStyle myTextStyle = TextStyle(
+//     fontSize: 17,
+//     color: Colors.white,
+//     fontWeight: FontWeight.bold,
+//   );
+// }
 
-class SettingValueTextStyle {
-  static final TextStyle myTextStyle = TextStyle(
-    color: Colors.white,
-    fontSize: 17,
-  );
-}
+// class SettingValueTextStyle {
+//   static final TextStyle myTextStyle = TextStyle(
+//     color: Colors.white,
+//     fontSize: 17,
+//   );
+// }
 
 
 
 
 // Q＆A用のウィジェット
-class QAWidget extends StatelessWidget {
+class qaWidget extends StatelessWidget {
   final String qText;
   final String aText;
 
-  const QAWidget({
+  const qaWidget({
     Key? key,
     required this.qText,
     required this.aText,
@@ -118,80 +102,107 @@ class QAWidget extends StatelessWidget {
     return Wrap(
       children: [
         Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFF7d7d7d),
-                  ),
-                  padding: EdgeInsets.all(5),
-                  margin: EdgeInsets.all(25),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 2.0),
-                              child: Icon(
-                                Icons.help_outline,
-                                color: Colors.white,
-                                size: 24,
-                              ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 8.0),
-                                child: Text(
-                                  qText,
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: qaBackgroundColor(),
+          ),
+          padding: EdgeInsets.all(5),
+          margin: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 2.0),
+                      child: Icon(
+                        Icons.help_outline,
+                        color: qaContentsColor(),
+                        size: qaIconSize(),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          qText,
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: qaTextSize(),
+                            color: qaContentsColor(),
+                          ),
                         ),
                       ),
-                      Container(
-                        height: 1,
-                        color: Colors.white,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 3.0),
-                              child: Icon(
-                                Icons.chat_bubble_outline,
-                                color: Colors.white,
-                                size: 24,
-                              ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 8.0),
-                                child: Text(
-                                  aText,
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+              ),
+              Container(
+                height: 1,
+                color: qaContentsColor(),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 3.0),
+                      child: Icon(
+                        Icons.chat_bubble_outline,
+                        color: qaContentsColor(),
+                        size: qaIconSize(),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          aText,
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontSize: qaTextSize(),
+                            color: qaContentsColor(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
+
+
+
+  // QA部分の文字サイズを一括指定
+  double qaIconSize() {
+    final qaIconSize = 24.0;
+    return qaIconSize;
+  }
+
+  // QA部分の文字サイズを一括指定
+  double qaTextSize() {
+    final qaTextSize = 16.0;
+    return qaTextSize;
+  }
+
+  // アイコン・文字色・仕切り線の背景色を指定
+  Color qaContentsColor() {
+    final qaContentsColor = Colors.white;
+    return qaContentsColor;
+  }
+
+  // アイコン・文字色・仕切り線の背景色を指定
+  Color qaBackgroundColor() {
+    final qaBackgroundColor = Color(0xFF555555);
+    return qaBackgroundColor;
+  }
+
 }
